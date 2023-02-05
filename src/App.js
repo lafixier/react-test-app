88021743fc783521d53767061a8b61568a6a2092
lafixier <lfxr@proton.me>
2023-02-05 22:15:34 +0900

    reactを高速回転

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>超高速回転するReact君</p>
         <a
           className="App-link"
           href="https://reactjs.org"
